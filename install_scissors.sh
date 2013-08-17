0e167ec302b27d324b21076561837ab3ce545136
Drew Crawford <drew@sealedabstract.com>
2013-08-17 00:59:31 -0500

    updating install script

--- install_scissors.sh
+++ install_scissors.sh
@@ -1,7 +1,7 @@
 # The contents of this file are licensed to you under the Reciprical Public License (RPL).  See the LICENSE file for details.
 #this script installs scissors
 set -e
-apt-get install python3 python3-pip
+apt-get install -y python3 python3-pip
 pip install fabric
 wget https://github.com/drewcrawford/scissors/archive/master.zip
 unzip master.zip -d scissors
